db607a22027ace156f4edb90674a8c9bdea0f985
lizenbyj <lizenbyjmsu@gmail.com>
2013-07-31 21:01:42 +0000

    fixed error in require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server = require("./server");
 var router = require("./router");
-var requestHandlers = ("./requestHandlers");
+var requestHandlers = require("./requestHandlers");
 
 var handle = {}
 handle["/"] = requestHandlers.start;
